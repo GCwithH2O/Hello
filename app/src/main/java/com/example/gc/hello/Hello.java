030f77aadd5829890bb3e429962041e052155fd2
GC <gaochen229@163.com>
2018-05-05 17:05:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/gc/hello/Hello.java
@@ -0,0 +1,13 @@
+package com.example.gc.hello;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Hello extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello);
+    }
+}
